f1279ee66a9dfbacec295d46e58820ae013b73e8
Maik Derksen <maikderksen2005@gmail.com>
2025-07-16 11:20:55 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/components/rich-text-editor.tsx
+++ src/components/rich-text-editor.tsx
@@ -1,6 +1,7 @@
 
 'use client';
 
+import * as React from 'react';
 import { useEditor, EditorContent, type Editor } from '@tiptap/react';
 import StarterKit from '@tiptap/starter-kit';
 import { Bold, Italic, List, ListOrdered } from 'lucide-react';
